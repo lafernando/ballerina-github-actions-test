ec5818ac53c7c82b6d808a649cf41ca44d4cd466
L.A. Fernando <lafernando@gmail.com>
2019-03-09 03:30:31 -0800

    Update

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -5,6 +5,7 @@ ls -la
 sh -c "echo $*"
 ls -la /ballerina/runtime
 echo $BALLERINA_HOME
+mkdir ~/.ballerina
 echo "XXX" > ~/.ballerina/Settings.toml
 cat ~/.ballerina/Settings.toml
 $BALLERINA_HOME/bin/ballerina --version
